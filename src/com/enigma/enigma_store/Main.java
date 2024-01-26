3d6e779f15becd3ece1156faad56e9c1bfe2791a
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-01-26 16:52:17 +0700

    [FIX] Fix Project

--- src/com/enigma/enigma_store/Main.java
+++ src/com/enigma/enigma_store/Main.java
@@ -4,6 +4,7 @@ package com.enigma.enigma_store;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("FIX Project");
         Menu menu=new Menu();
         menu.showMenu();
 
